6841bd11512948ad3a5a3eb7e13ca8e9105a60c7
Samuel Eli Heletsi <83101355+Samuelheletsi@users.noreply.github.com>
2025-08-29 09:21:31 -0400

    up1

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ function populateTeamSections(data){
     {id:"pastors", items:data.pastors, fields:["description"]},
     {id:"fellowships", items:data.fellowships, fields:["leader_name","assistant"]},
     {id:"leaders", items:data.leaders, fields:["role"]},
-    {id:"awards", items:[data.awards.best_leader, data.awards.best_fellowship], fields:["description"]}
+    {id:"awards", items:[...data.awards.best_leaders, ...data.awards.best_fellowships], fields:["description"]}
   ];
 
   sections.forEach(s=>{
